b37a89b51bfb11c7d8cbe6556c8ee49b3eb50ddd
confy <confy>
2020-11-29 21:52:42 -0800

    switching computers - some of it works!

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const html = document.querySelector("html")
 const content = document.querySelector(".content")
 const textarea = document.querySelector(".content textarea")
+const errortxt = document.querySelector("#error")
+const nav = document.querySelector(".nav")
 const menus = document.querySelectorAll(".header, .footer, .nav")
 const buttons = document.querySelectorAll("button")
 const btnNewNote = document.querySelector("#new")
@@ -9,10 +11,13 @@ const btnTheme = document.querySelector("#changeTheme")
 const btnCancel = document.querySelector("#cancel")
 const btnSave = document.querySelector("#save")
 
+
 let notesArray = [
-    {title:"note one", body:"some text 1"},
-    {title:"note two", body:"some text 2"}
+    { title: "note one", body: "some text 1" },
+    { title: "note two", body: "some text 2" }
 ]
+
+
 //Dark or light mode toggle
 const theme = {
     html: ["#F0F0EE", "#2E3440"],
@@ -25,25 +30,41 @@ const theme = {
     btnBorder: ["2px solid #203975", "2px solid #2E3440"],
     darkOrLight: ["Dark Mode", "Light Mode"],
 }
+
 let count = 0
+
 btnTheme.addEventListener("click", () => {
     count++
     let themeNo = count % 2
-    btnTheme.innerHTML = theme["darkOrLight"][themeNo]
-    html.style.background = theme["html"][themeNo]
-    content.style.background = theme["content"][themeNo]
-    textarea.style.background = theme["textAreaBg"][themeNo]
-    textarea.style.color = theme["textAreaTxt"][themeNo]
+    btnTheme.innerHTML = theme.darkOrLight[themeNo]
+    html.style.background = theme.html[themeNo]
+    content.style.background = theme.content[themeNo]
+    textarea.style.background = theme.textAreaBg[themeNo]
+    textarea.style.color = theme.textAreaTxt[themeNo]
+
     for (i = 0; i < menus.length; i++) {
-        menus[i].style.background =  theme["menus"][themeNo]
+        menus[i].style.background = theme.menus[themeNo]
     }
+
     for (i = 0; i < buttons.length; i++) {
-        buttons[i].style.color = theme["btnTxt"][themeNo]
-        buttons[i].style.background = theme["btnBg"][themeNo]
-        buttons[i].style.border = theme["btnBorder"][themeNo]
+        buttons[i].style.color = theme.btnTxt[themeNo]
+        buttons[i].style.background = theme.btnBg[themeNo]
+        buttons[i].style.border = theme.btnBorder[themeNo]
     }
 })
 
+function toggleContent(visibility) {
+    if (visibility == false) {
+        textarea.style.visibility = "hidden"
+        btnSave.style.visibility = "hidden"
+        btnCancel.style.visibility = "hidden"
+        errortxt.style.visibility = "hidden"
+    } else {
+        textarea.style.visibility = "visible"
+        btnSave.style.visibility = "visible"
+        btnCancel.style.visibility = "visible"
+    }
+}
 
 hidden = false
 btnCancel.addEventListener("click", () => {
@@ -51,7 +72,51 @@ btnCancel.addEventListener("click", () => {
     hidden = true
 })
 
-btnNewNote.addEventListener("click", () =>{
+
+//Checks ands sees if note exists in array
+function checkNote(title) {
+    for (i = 0; i < notesArray.length; i++) {
+        if (notesArray[i].title == title) {
+            return false
+        }
+    }
+    return true
+}
+function createBtn(title) {
+}
+function addToArray(title, body) {
+}
+
+//Appends error txt to content
+function error(show) {
+    if (show == true) {
+        errortxt.style.visibility = "visible"
+    } else {
+        errortxt.style.visibility = "hidden"
+    }
+}
+
+// Save button
+btnSave.addEventListener("click", () => {
+    if (textarea.value !== "") {
+        let bodyLines = textarea.value.split("/n")
+        let title = bodyLines[0].toLowerCase()
+        if (checkNote(title) == false) {
+            error(true)
+            console.log("dupe")
+            return null
+        }
+        let body = ""
+        for (i = 1; i < bodyLines; i++) {
+            body += `${bodyLines[i]}\n`
+        }
+    } else {
+        return null
+    }
+})
+
+// New Note button
+btnNewNote.addEventListener("click", () => {
     if (hidden == true) {
         toggleContent(true)
         hidden = false
@@ -59,14 +124,8 @@ btnNewNote.addEventListener("click", () =>{
         textarea.value = ""
     }
 })
-function toggleContent(visibility) {
-    if (visibility == false){
-        textarea.style.visibility = "hidden"
-        btnSave.style.visibility = "hidden"
-        btnCancel.style.visibility = "hidden"
-    } else {
-        textarea.style.visibility = "visible"
-        btnSave.style.visibility = "visible"
-        btnCancel.style.visibility = "visible"
-    }
-}
+
+
+
+
+
